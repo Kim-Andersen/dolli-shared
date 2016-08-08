fcf30a4318b6d20e31d52fcf8bf85b85d7726ff7
Kim-Andersen <kimandersen1@gmail.com>
2016-08-08 10:23:08 +0200

    Types

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+// @flow
+
 'use strict;'
 
 var format = require('./lib/format');
 var validate = require('./lib/validate');
 var models = require('./lib/models');
+// const TypeDefinitions = require('./lib/TypeDefinitions');
 
 module.exports.format = format;
 module.exports.validate = validate;
 module.exports.models = models;
+// module.exports.TypeDefinitions = TypeDefinitions;
+
+export * from './lib/TypeDefinitions';
+
+// const defaultExport = {...TypeDefinitions};
+// module.exports = defaultExport;
---
+++ test/test-TypeDefinitions.js
@@ -0,0 +1,17 @@
+'use strict;'
+
+var expect = require('chai').expect;
+
+// import type { Account } from '../lib/TypeDefinitions';
+//
+// console.log('require', require('../lib/TypeDefinitions'));
+
+describe.skip('TypeDefinitions', function() {
+
+  // console.log('Account', Account);
+
+  // it('should export expected types', () => {
+  //   expect(types).to.include('Account');
+  // });
+
+});
